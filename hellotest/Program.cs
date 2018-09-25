dcb465c7c1349f6f3bb852cd83df28d9d2f60455
christensenjoshua <christensen.joshua@outlook.com>
2018-09-24 20:00:34 -0600

    Basic functionality.

--- hellotest/Program.cs
+++ hellotest/Program.cs
@@ -10,6 +10,37 @@ namespace hellotest
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int numToGuess = rnd.Next(1, 10);
+            int numTries = 0;
+            Console.WriteLine("Number Guessing Game!");
+            bool found = false;
+            while(found == false)
+            {
+                Console.Write("Please guess a number 1 through 10: ");
+                string userInput = Console.ReadLine();
+                int userNum = Int32.Parse(userInput);
+                if(userNum == numToGuess)
+                {
+                    numTries++;
+                    found = true;
+                    Console.WriteLine("You guessed correctly in "+numTries+" attempts.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    numTries++;
+                    Console.WriteLine("Incorrect, try again!");
+                    if(userNum > numToGuess)
+                    {
+                        Console.WriteLine("Your number was too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number was too low.");
+                    }
+                }
+            }
         }
     }
 }
